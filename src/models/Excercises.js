8949831ea5c0cdc4d0076a9c7262308e30f8132c
pablolospe <yosoypxl@gmail.com>
2022-11-07 22:00:05 -0300

    excercises model 1

---
+++ src/models/Excercises.js
@@ -0,0 +1,22 @@
+const { Datatypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('excercises', {
+        name: {
+            type: Datatypes.STRING,
+            allowNull: false,
+        },
+        series: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        repetitions: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+        {
+            timestamps: false,
+        }
+    )
+}
