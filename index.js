558f8b7d7eb49946f32bbec7612a75a29b26bf7e
Martin Galara <mgalara@gmail.com>
2022-11-10 11:13:06 -0300

    asd

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ require('dotenv').config();
 const { PORT } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     const users = [
