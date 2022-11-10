01a39ce3b70cca8ebd38068e05254fd100e5fa83
Martin Galara <mgalara@gmail.com>
2022-11-10 10:46:30 -0300

    deploy

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const { User , Routine , Day, Excercise, Muscle, Product, Membresy, Class} = req
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(process.env.PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
     const users = [
       {name: "Martin Galara", email: "mgalara@gmail.com", hashPassword: "$2b$08$/DFujLqVmZYc2qHWRdf.EuXZTLOlf2NzuL5ihfcJ0xkR/5vH7Fk/e",role: "User"},      //123
