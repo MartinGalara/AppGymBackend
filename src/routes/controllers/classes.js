d09d97604ee474f882b355964f36d55f9684842d
Gaston Schmitz <108362008+Schmitz0@users.noreply.github.com>
2022-11-27 15:25:52 -0300

    cambio en clases para manu

--- src/routes/controllers/classes.js
+++ src/routes/controllers/classes.js
@@ -31,12 +31,14 @@ router.get('/', userExtractor, async (req, res) => {
 
 router.post('/', userExtractor, async (req, res) => {
     try {
-        let { name, date, id } = req.body;
-        if (!name || !date) return res.status(400).json('Missing inputs')
+        let { name, date, id, hour,day } = req.body;
+        if (!name || !date || !hour || !day) return res.status(400).json('Missing inputs')
 
         const newClass = await Class.create({
             name,
             date,
+            hour,
+            day,
             // id,
         });
 
